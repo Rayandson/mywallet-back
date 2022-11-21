c11a37b2608aaf7152a34d7a4955a81e8f770afa
Rayandson <rayandson.silva321@gmail.com>
2022-11-21 12:15:38 -0300

    alter port

--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use(authRoutes);
 app.use(userRoutes);
 
-const port = 5000;
-app.listen(port, () => console.log(`Server running in port: ${port}`));   
+const port = process.env.port || 5000;
+app.listen(port, () => console.log(`Server running in port: ${port}`));    
